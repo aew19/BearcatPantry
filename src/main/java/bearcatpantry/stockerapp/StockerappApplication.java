231ae16d9369586780e4208191c4e47c53614fb9
ashley <wilhelay@mail.uc.edu>
2020-09-14 13:39:03 -0400

    Initalize

---
+++ src/main/java/bearcatpantry/stockerapp/StockerappApplication.java
@@ -0,0 +1,13 @@
+package bearcatpantry.stockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockerappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockerappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bearcatpantry/stockerapp/StockerappApplicationTests.java
@@ -0,0 +1,13 @@
+package bearcatpantry.stockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockerappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
