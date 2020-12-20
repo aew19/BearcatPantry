8b76f9bd21b990f5444bf43cc59e41d99e58c763
ashley <wilhelay@mail.uc.edu>
2020-12-19 22:04:20 -0500

    Added apache tomcat

--- server/src/main/java/com/bcpstockerapp/bcp/BcpApplication.java
+++ server/src/main/java/com/bcpstockerapp/bcp/BcpApplication.java
@@ -2,9 +2,11 @@ package com.bcpstockerapp.bcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class BcpApplication {
+public class BcpApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(BcpApplication.class, args);
