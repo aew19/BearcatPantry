6fb3b9e1108424e78e726dc6209feefafd7fb7fb
wharfratcat <vanmetjk@mail.uc.edu>
2020-12-22 20:40:41 -0500

    Created images folder and began working on student landing page

---
+++ frontend/student/student.js
@@ -0,0 +1,19 @@
+//This function just loads the navbar onto the page
+$(function(){
+    $("#navBar").load("../navBar.html");
+});
+
+function loadPopularItems(items){
+    const table = document.getElementById("popItems");
+    items.forEach(item => {
+        let row = table.insertRow();
+        let name = row.insertCell(0);
+        name.innerHTML = item.name;
+    });
+}
+
+const items = [
+    {name: "Pasta", quantity: 10, type:"Grains", brand: "Kroger", vegOrVeg: "Vegetarian", bestBuy:"11/09/2020", expiration:"11/09/2020"},
+    {name: "Tomatos", quantity: 20, type:"Vegtable", brand: "Walmart", vegOrVeg: "Vegan", bestBuy:"11/10/2020", expiration:"11/10/2020"}
+];
+loadPopularItems(items);
