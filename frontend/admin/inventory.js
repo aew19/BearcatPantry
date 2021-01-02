6db934513beb65a857276f135e49dbd1a8f113d9
Ashley <ashleywilhelm1919@gmail.com>
2021-01-02 18:22:00 -0500

    Added styling to inventory table

--- frontend/admin/inventory.js
+++ frontend/admin/inventory.js
@@ -102,23 +102,25 @@ function exportCSV(elem){
 
 function loadPantryItems(items){
     const table = document.getElementById("pantryStock");
-    items.forEach(item => {
+    for (let element of items) {
         let row = table.insertRow();
-        let name = row.insertCell(0);
-        name.innerHTML = item.name;
-        let quantity = row.insertCell(1);
-        quantity.innerHTML = item.quantity;
-        let type =  row.insertCell(2);
-        type.innerHTML = item.type;
-        let brand = row.insertCell(3);
-        brand.innerHTML = item.brand;
-        let vegOrVeg =  row.insertCell(4);
-        vegOrVeg.innerHTML = item.vegOrVeg;
-        let bestBuy =  row.insertCell(5);
-        bestBuy.innerHTML = item.bestBuy;
-        let expiration = row.insertCell(6);
-        expiration.innerHTML = item.expiration;
-    });
+        for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            if (element[key] < 15) {
+                cell.style.backgroundColor = '#ff3823';
+                cell.style.color = '#fff';
+            }
+            else if (element[key] >= 15 & element[key] < 45) {
+                cell.style.backgroundColor = '#fefb64';
+            }
+            else if (element[key] >= 45) {
+                cell.style.backgroundColor = '#92d36e';
+                cell.style.color = '#fff';
+            }
+            cell.appendChild(text);
+        }
+    }
 }
 const items = [
     {name: "Pasta", quantity: 10, type:"Grains", brand: "Kroger", vegOrVeg: "Vegetarian", bestBuy:"11/09/2020", expiration:"11/09/2020"},
