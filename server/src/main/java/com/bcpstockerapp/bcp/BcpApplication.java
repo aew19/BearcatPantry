bcac90b4ccead19d23cd492b55dc2a59aa42d643
ashley <wilhelay@mail.uc.edu>
2021-01-13 19:29:31 -0500

    Added tomcat requirements

--- server/src/main/java/com/bcpstockerapp/bcp/BcpApplication.java
+++ server/src/main/java/com/bcpstockerapp/bcp/BcpApplication.java
@@ -3,12 +3,18 @@ package com.bcpstockerapp.bcp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
 public class BcpApplication extends SpringBootServletInitializer{
-
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(BcpApplication.class);
+    }
+    public static void main(String[] args)
+    {
         SpringApplication.run(BcpApplication.class, args);
     }
 
