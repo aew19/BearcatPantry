bb3647615dbffb7442a3beacb8e4a9701123d67c
ashley <wilhelay@mail.uc.edu>
2021-01-22 14:11:17 -0500

    Updated UsersTable

--- server/src/main/java/com/bcpstockerapp/bcp/model/UsersTable.java
+++ server/src/main/java/com/bcpstockerapp/bcp/model/UsersTable.java
@@ -11,16 +11,18 @@ public class UsersTable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
+    private String fname;
+    private String lname;
     private Integer permissions;
     private boolean isActive;
     private Date dateActive;
 
     //Constructors
     public UsersTable(){}
-    public UsersTable(Long id, String name, Integer permissions, boolean isActive, Date dateActive){
+    public UsersTable(Long id, String fname, String lname, Integer permissions, boolean isActive, Date dateActive){
         this.id = id;
-        this.name = name;
+        this.fname = fname;
+        this.lname = lname;
         this.permissions = permissions;
         this.isActive = isActive;
         this.dateActive = dateActive;
@@ -28,14 +30,16 @@ public class UsersTable {
 
     //Getters
     public Long getId(){return this.id;}
-    public String getName(){return this.name;}
+    public String getFname(){return this.fname;}
+    public String getLname(){return this.lname;}
     public Integer getPermissions(){return this.permissions;}
     public boolean getIsActive(){return this.isActive;}
     public Date getDateActive(){return this.dateActive;}
 
     //Setters
     public void setId(Long id){this.id = id;}
-    public void setName(String name){this.name = name;}
+    public void setFname(String fname){this.fname = fname;}
+    public void setLname(String lname){this.lname = lname;}
     public void setPermissions(Integer permissions){this.permissions = permissions;}
     public void setIsActive(boolean isActive){this.isActive = isActive;}
     public void setDateActive(Date dateActive){this.dateActive = dateActive;}
