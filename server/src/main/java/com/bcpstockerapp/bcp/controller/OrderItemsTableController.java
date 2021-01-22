06d805863a74dfa02f11974877077412a3ed20fd
ashley <wilhelay@mail.uc.edu>
2021-01-22 16:33:34 -0500

    Added Order Items Table

---
+++ server/src/main/java/com/bcpstockerapp/bcp/controller/OrderItemsTableController.java
@@ -0,0 +1,29 @@
+package com.bcpstockerapp.bcp.controller;
+
+import com.bcpstockerapp.bcp.model.OrderItemsTable;
+import com.bcpstockerapp.bcp.repository.OrderItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class OrderItemsTableController {
+    @Autowired
+    private OrderItemsRepository orderItemsRepository;
+
+    @GetMapping("/orderItems")
+    public @ResponseBody ResponseEntity<List<OrderItemsTable>> getAllOrderItems(){
+        try{
+            List<OrderItemsTable> orderItems = orderItemsRepository.findAll();
+            return new ResponseEntity<>(orderItems, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ server/src/main/java/com/bcpstockerapp/bcp/model/OrderItemsTable.java
@@ -0,0 +1,38 @@
+package com.bcpstockerapp.bcp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class OrderItemsTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long orderItemID;
+    private Long orderId;
+    private Long inventoryId;
+    private Integer itemQuantity;
+
+    //Constructors
+    OrderItemsTable(){};
+    OrderItemsTable(Long orderItemID, Long orderId, Long inventoryId, Integer itemQuantity){
+        this.orderItemID = orderItemID;
+        this.orderId = orderId;
+        this.inventoryId = inventoryId;
+        this.itemQuantity = itemQuantity;
+    }
+
+    //Getters
+    public Long getOrderItemID(){return this.orderItemID;}
+    public Long getOrderId(){return this.orderId;}
+    public Long getInventoryId(){return this.inventoryId;}
+    public Integer getItemQuantity(){return this.itemQuantity;}
+
+    //Setters
+    public void setOrderId(Long orderId){this.orderId = orderId;}
+    public void setInventoryId(Long inventoryId){this.inventoryId = inventoryId;}
+    public void setItemQuantity(Integer itemQuantity){this.itemQuantity = itemQuantity;}
+
+}
