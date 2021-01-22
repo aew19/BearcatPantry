6f1c0afe172f504bd40ea057dcb66b935288daf3
ashley <wilhelay@mail.uc.edu>
2021-01-22 15:59:32 -0500

    Updated InventoryTable

--- server/src/main/java/com/bcpstockerapp/bcp/model/InventoryTable.java
+++ server/src/main/java/com/bcpstockerapp/bcp/model/InventoryTable.java
@@ -11,28 +11,36 @@ import java.util.Date;
 public class InventoryTable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long inventoryId;
 
     private Long productId;
+    private Integer quantity;
+    private String location;
     private Date dateRecorded;
     private Date expirationDate;
 
     //Constructors
     public InventoryTable(){}
-    public InventoryTable(Long productId, Date dateRecorded, Date expirationDate){
+    public InventoryTable(Long productId, Integer quantity, String location, Date dateRecorded, Date expirationDate){
         this.productId = productId;
+        this.quantity = quantity;
+        this.location = location;
         this.dateRecorded = dateRecorded;
         this.expirationDate = expirationDate;
     }
 
     //Getters
-    public Long getId(){return this.id;}
+    public Long getId(){return this.inventoryId;}
     public Long getProductId(){return this.productId;}
+    public Integer getQuantity(){return this.quantity;}
+    public String getLocation(){return this.location;}
     public Date getDateRecorded(){return this.dateRecorded;}
     public Date getExpirationDate(){return this.expirationDate;}
 
     //Setters
     public void setProductId(Long productId){this.productId = productId;}
+    public void setQuantity(Integer quantity){this.quantity = quantity;}
+    public void setLocation(String location){this.location = location;}
     public void setDateRecorded(Date dateRecorded){this.dateRecorded = dateRecorded;}
     public void setExpirationDate(Date expirationDate){this.expirationDate = expirationDate;}
 
