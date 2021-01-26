3e4a8f02cff927636eada907d8d8f51faebf88be
wharfratcat <W#@RFR@TCat1423#>
2021-01-26 08:29:37 -0500

    Added Item shopping cart class

---
+++ server/src/main/java/com/bcpstockerapp/bcp/model/ShoppingCart.java
@@ -0,0 +1,39 @@
+package main.java.com.bcpstockerapp.bcp.model;
+
+public class ShoppingCart {
+    private String itemName;
+    private Integer itemBarcode;
+    private Integer itemQuantity;
+
+    public ShoppingCart(String itemName, Integer itemBarcode, Integer itemQuantity) {
+        this.itemName = itemName;
+        this.itemBarcode = itemBarcode;
+        this.itemQuantity = itemQuantity;
+    }
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getItemBarcode() {
+        return itemBarcode;
+    }
+
+    public void setItemBarcode(Integer itemBarcode) {
+        this.itemBarcode = itemBarcode;
+    }
+
+    public Integer getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(Integer itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+}
