5f1f137571e3077ca400c1e9271a79eb334e46b0
ashley <wilhelay@mail.uc.edu>
2021-02-17 11:24:18 -0500

    Image Preview is in the inventory.js file

--- frontend/admin/inventory.js
+++ frontend/admin/inventory.js
@@ -473,5 +473,17 @@ function createTableStyle() {
     });
 }
 
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('#imgPreview').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+}
+
 createInventoryTable()
 // loadBulkScan(bulkitems);
