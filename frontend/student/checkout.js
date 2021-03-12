44ef030861d5a374322f71c6aa27384ad39367f3
ashley <wilhelay@mail.uc.edu>
2021-03-12 12:15:58 -0500

    Added delete checkout functionality

--- frontend/student/checkout.js
+++ frontend/student/checkout.js
@@ -6,8 +6,19 @@ function submitOrder() {
     //Hit orders post
 }
 
-function removeItemInCart(itemId) {
-
+function removeItemInCart(barcode) {
+    console.log(sessionStorage)
+    let cart = sessionStorage.getItem('cart')
+    let cartArray = cart.split("::")
+    let index = cartArray.indexOf(barcode)
+    if (index > -1){
+        cartArray.splice(index,1);
+    }
+    let newCart = cartArray.join("::")
+    console.log(newCart)
+    sessionStorage.setItem('cart',newCart)
+    console.log(sessionStorage)
+    location.reload()
 }
 
 async function getBarcode(barcode){
@@ -21,11 +32,22 @@ async function getBarcode(barcode){
 
 function populateCart(){
     let items = sessionStorage.getItem('cart')
+    console.log("HITTTT!!!")
+    console.log(items)
+    if (items === ""){
+        sessionStorage.removeItem('cart')
+        return;
+    }
     let barcodes = items.split('::')
     barcodes.forEach(barcode =>{
+        if (barcode=== ""){
+            return;
+        }
         getBarcode(barcode).then(data =>{
-            console.log(data)
-            let currentElement = JSON.stringify(data.barcodeId)
+            if (data === undefined){
+                return;
+            }
+            let currentElement = JSON.stringify(barcode)
             console.log(data)
             let listHousing = document.getElementById("itemList")
             let item = document.createElement("li")
@@ -38,14 +60,12 @@ function populateCart(){
             itemBrand.innerHTML="<small class=\"text-muted\">"+data.brand+"</small>"
             storageDiv.appendChild(itemBrand)
             let btnDeleteItem = document.createElement("a")
-            btnDeleteItem.innerHTML = "<a class=\"btn btn-red\" id=\"CheckoutDeleteBtn\" onclick=\"removeItemInCart()\"><i class=\"fas fa-trash\" aria-hidden=\"true\"></i></a>"
+            btnDeleteItem.innerHTML = "<a class=\"btn btn-red\" id=\"CheckoutDeleteBtn\" onclick=removeItemInCart("+currentElement+")><i class=\"fas fa-trash\" aria-hidden=\"true\"></i></a>"
             item.appendChild(storageDiv)
             item.appendChild(btnDeleteItem)
             listHousing.appendChild(item)
         })
     })
-    //get cart items
-    //then format them
 }
 let url ="";
 let posturl = "";
