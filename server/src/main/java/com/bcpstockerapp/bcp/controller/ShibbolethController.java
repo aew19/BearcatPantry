b651487d131140cbb240028c2368fbc1aceae00e
lindsaln <loganlindsay59@gmail.com>
2021-03-29 00:10:04 -0400

    testing with shib

--- server/src/main/java/com/bcpstockerapp/bcp/controller/ShibbolethController.java
+++ server/src/main/java/com/bcpstockerapp/bcp/controller/ShibbolethController.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -18,10 +21,29 @@ public class ShibbolethController {
     @Autowired
     private HttpServletRequest request;
 
+    public static void ShibbolethController() {
+        
+    }
+
     @GetMapping("/mail")
     public @ResponseBody String getAllHeaders(){
         String mail = request.getHeader("mail");
         System.out.println(mail);
         return mail;
+    }
+
+    @GetMapping("/shibtest")
+    public @ResponseBody Map getRequestHeaderValues() {
+        Map map = new HashMap();
+                // get header values from request object
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+            System.out.println(key + " - " + value);
+        }
+        
+        return map;
     }
 }
