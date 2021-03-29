06bf5cdc28bed56715415e042f46fcef00ccc69f
lindsaln <loganlindsay59@gmail.com>
2021-03-29 18:18:44 -0400

    shibboleth fix

--- server/src/main/java/com/bcpstockerapp/bcp/controller/ShibbolethController.java
+++ server/src/main/java/com/bcpstockerapp/bcp/controller/ShibbolethController.java
@@ -1,6 +1,5 @@
 package com.bcpstockerapp.bcp.controller;
 
-import com.bcpstockerapp.bcp.model.ShibbolethData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
